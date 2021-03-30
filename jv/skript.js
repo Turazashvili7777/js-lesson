20ab8a8876649ec853e3b2deb529694108dcbcfa
Turazashvili7777 <turazashvili2021@mail.ru>
2021-03-30 13:43:17 +0600

    lesson

---
+++ jv/skript.js
@@ -0,0 +1,50 @@
+let str1 = `Привет,`
+let str2 = `Мир!`
+    // document.write(`<h3>n1 ${str1} n2 ${str2} </h3>`)
+document.write(`<p>${str1} + ${str2} = ${str1 + str2}</p>`)
+
+
+
+var chs1 = 2
+var chs2 = 60
+var chs3 = 60
+
+document.write(`<p>${chs1} * ${chs2} * ${chs3} = ${chs1 * chs2 * chs3}</p>`)
+
+
+
+let a = 10
+let b = 2
+
+document.write(`<p>${a} + ${b} = ${a + b}</p>`)
+document.write(`<p>${a} - ${b} = ${a - b}</p>`)
+document.write(`<p>${a} * ${b} = ${a * b}</p>`)
+document.write(`<p>${a} / ${b} = ${a / b}</p>`)
+
+// document.write(`<p>${a} % ${b} = ${a % b}</p>`)
+
+var c = +(prompt(``))
+var d = +(prompt(``))
+
+document.write(`<p>${c} + ${d} = ${c + d}</p>`)
+document.write(`<p>${c} - ${d} = ${c - d}</p>`)
+document.write(`<p>${c} * ${d} = ${c * d}</p>`)
+document.write(`<p>${c} / ${d} = ${c / d}</p>`)
+    // 
+
+let f = 10
+let p = 2
+let w = 5
+
+document.write(`<p>${f} + ${p} + ${w} = ${a + p + w}</p>`)
+
+
+
+var q = 8
+var l = 9
+var o = 5
+var r = 3
+
+
+
+document.write(`<p>(${q} + ${l} + ${o}) / ${r} = ${(c + d + o) / r}</p>`)
